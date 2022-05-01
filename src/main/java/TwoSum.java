e0f8032cf7e9a97f0f25c83eff3d3e3461e574b5
engkimbs <eng.kimbs@gmail.com>
2022-05-01 11:28:14 +0900

    TwoSum

---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] result = twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+            map.put(numbers[i], i);
+        }
+        return result;
+    }
+}
