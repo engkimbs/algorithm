83a18c409420b8ff39272922bd1becc082abd7e1
engkimbs <eng.kimbs@gmail.com>
2022-05-16 23:46:41 +0900

    ContainerWithMostWaterr

---
+++ src/main/java/ContainerWithMostWater.java
@@ -0,0 +1,25 @@
+public class ContainerWithMostWater {
+
+    public static int maxArea(int[] height) {
+        int result = 0;
+        int left=0, right = height.length-1;
+
+        while(left < right) {
+            int leftHeight = height[left];
+            int rightHeight = height[right];
+
+            int area = (right - left)*Math.min(leftHeight, rightHeight);
+            result = Math.max(result, area);
+
+            if(leftHeight >= rightHeight) --right;
+            if(leftHeight < rightHeight) ++left;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] heights = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(heights));
+    }
+}
